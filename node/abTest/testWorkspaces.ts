8c85ec704e25b06793ae15908103dd699840eeb7
lucas-daher <lucasdaher@Lucass-MBP.vtexlocal>
2019-12-18 19:46:48 -0300

    get granular data and call revenue evaluation

--- node/abTest/testWorkspaces.ts
+++ node/abTest/testWorkspaces.ts
@@ -3,17 +3,20 @@ import TestingWorkspaces from '../typings/testingWorkspace'
 import { DefaultEvaluationResponse } from '../utils/evaluation-response'
 import { FilteredWorkspacesData } from '../utils/workspace'
 import { EvaluateConversion } from './analysis/compareWorkspaces/conversion'
+import { EvaluateRevenue } from './analysis/compareWorkspaces/revenue'
 import { BuildCompleteData } from './data/buildData'
+import { TestType } from '../clients/vbase';
 
 const MasterWorkspaceName = 'master'
 
 export async function TestWorkspaces(account: string, abTestBeginning: string, workspacesMetadata: ABTestWorkspacesMetadata, ctx: Context): Promise<TestResult[]> {
-    const { clients: { abTestRouter, storedash } } = ctx
+    const { clients: { abTestRouter, storedash, storage } } = ctx
     const testingWorkspaces = new TestingWorkspaces(workspacesMetadata)
     const Results: TestResult[] = []
 
     if (testingWorkspaces.Length() > 0) {
-        const workspacesData = await FilterWorkspacesData(abTestBeginning, testingWorkspaces.WorkspacesNames(), storedash)
+        const testType = await storage.getTestType(ctx)
+        const workspacesData = await FilterWorkspacesData(abTestBeginning, testingWorkspaces.WorkspacesNames(), storedash, testType)
 
         if (!HasWorkspacesData(workspacesData)) {
             for (const workspaceName of testingWorkspaces.WorkspacesNames()) {
@@ -28,15 +31,20 @@ export async function TestWorkspaces(account: string, abTestBeginning: string, w
 
         for (const workspaceData of workspacesCompleteData) {
             if (workspaceData[0] !== MasterWorkspaceName) {
-                Results.push(await EvaluateConversion(abTestBeginning, masterWorkspace!, workspaceData[1]))
+                if (testType === TestType.conversion) {
+                    Results.push(await EvaluateConversion(abTestBeginning, masterWorkspace!, workspaceData[1]))
+                }
+                if (testType === TestType.revenue) {
+                    Results.push(await EvaluateRevenue(abTestBeginning, masterWorkspace!, workspaceData[1]))
+                }
             }
         }
     }
     return Results
 }
 
-async function FilterWorkspacesData(aBTestBeginning: string, testingWorkspaces: string[], storedash: Storedash): Promise<WorkspaceData[]> {
-    const workspacesData = await storedash.getWorkspacesData(aBTestBeginning)
+async function FilterWorkspacesData(aBTestBeginning: string, testingWorkspaces: string[], storedash: Storedash, testType: TestType): Promise<WorkspaceData[]> {
+    const workspacesData = (testType === TestType.revenue) ? (await storedash.getWorkspacesGranularData(aBTestBeginning)).data : await storedash.getWorkspacesData(aBTestBeginning)
     return FilteredWorkspacesData(workspacesData, testingWorkspaces)
 }
 
